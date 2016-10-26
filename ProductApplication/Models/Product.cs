3f057cb34c8819c5f2b897d86c6ff0e66b959352
Mazedur Rahman <Mazedur Rahman>
2016-10-26 16:59:51 +0600

    CRUD is done by ASP.NET MVC 5

---
+++ ProductApplication/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductApplication.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Decription { get; set; }
+
+    }
+}
